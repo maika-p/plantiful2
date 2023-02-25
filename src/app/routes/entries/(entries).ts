ea88491063d1762b204f8864557900a6648a5190
Miika <miika.pollari97@gmail.com>
2023-02-25 18:27:29 +0200

    testing gitignore

--- src/app/routes/entries/(entries).ts
+++ src/app/routes/entries/(entries).ts
@@ -3,7 +3,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { EntriesActions, injectEntriesFeature } from '../../store/entries';
 import EntriesListComponent from '../../entries/entries-list/entries-list';
-
+import { Store } from '@ngrx/store/src';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
