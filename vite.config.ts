f36c2040cc186fd412a1bba379ab8cbb50021e75
Miika <miika.pollari97@gmail.com>
2023-03-05 17:37:23 +0200

    now packages work

--- vite.config.ts
+++ vite.config.ts
@@ -1,85 +1,59 @@
 /// <reference types="vitest" />
 
 import analog from '@analogjs/platform';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
-import { defineConfig, splitVendorChunkPlugin } from 'astro/config';
-
-// https://astro.build/config
-import analogjsangular from '@analogjs/astro-angular';
-
-// https://astro.build/config
-import node from '@astrojs/node';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [analogjsangular()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  vite: {
-    ssr: {
-      noExternal: ['@angular/**'],
-    },
-    define: {
-      ngDevMode: 'false',
-      ngJitMode: 'false',
-      ngI18nClosureMode: 'false',
-    },
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  publicDir: 'src/assets',
+  optimizeDeps: {
+    // include: [
+    //   '@angular/core',
+    //   '@angular/common',
+    //   '@angular/forms',
+    //   '@ngrx/store',
+    // ],
   },
-});
+  build: {
+    target: ['es2020'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
 
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   publicDir: 'src/assets',
-//   optimizeDeps: {
-//     // include: [
-//     //   '@angular/core',
-//     //   '@angular/common',
-//     //   '@angular/forms',
-//     //   '@ngrx/store',
-//     // ],
-//   },
-//   build: {
-//     target: ['es2020'],
-//   },
-//   resolve: {
-//     mainFields: ['module'],
-//   },
+  plugins: [
+    analog({
+      // ssr: true,
+      // static: true,
+      // ssrBuildDir: './dist/apps/plantiful2/ssr',
+      entryServer: './src/main.server.ts',
+      vite: {
+        inlineStylesExtension: 'scss',
+        tsconfig: './tsconfig.json',
+      },
+      nitro: {
+        rootDir: './',
+        output: {
+          dir: './dist/apps/plantiful2/server',
+          publicDir: './dist/apps/plantiful2/server/public',
+        },
+        buildDir: './dist/apps/plantiful2/.nitro',
+        // prerender: {
+        //   routes: ['/'],
+        // },
+      },
+    }),
 
-//   plugins: [
-//     analog({
-//       // ssr: true,
-//       // static: true,
-//       // ssrBuildDir: './dist/apps/plantiful2/ssr',
-//       entryServer: './src/main.server.ts',
-//       vite: {
-//         inlineStylesExtension: 'scss',
-//         tsconfig: './tsconfig.json',
-//       },
-//       nitro: {
-//         rootDir: './',
-//         output: {
-//           dir: './dist/apps/plantiful2/server',
-//           publicDir: './dist/apps/plantiful2/server/public',
-//         },
-//         buildDir: './dist/apps/plantiful2/.nitro',
-//         // prerender: {
-//         //   routes: ['/'],
-//         // },
-//       },
-//     }),
-
-//     // }
-//     // splitVendorChunkPlugin(),
-//   ],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//     setupFiles: ['src/test.ts'],
-//     include: ['**/*.spec.ts'],
-//   },
-//   define: {
-//     'import.meta.vitest': mode !== 'production',
-//   },
-// }));
+    // }
+    // splitVendorChunkPlugin(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test.ts'],
+    include: ['**/*.spec.ts'],
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
